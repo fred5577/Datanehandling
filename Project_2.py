b75824245e35e9988cf3f2dd88b6c2959f8b9c43
Zaggralez <jacob@pjetursson.com>
2018-01-12 09:31:52 +0100

    roundgrade done

--- Project_2.py
+++ Project_2.py
@@ -4,13 +4,9 @@ import numpy as np
 
 def roundGrade(grades):
     sevenStep = np.array([-3, 0, 2, 4, 7, 10, 12])
-    #print(np.abs(grades, sevenStep))
+    gradesRounded = []
     for grade in grades:
-        print(grade)
-        print(min(sevenStep, key=lambda x:abs(x-grade)))
-
-    gradesRounded = [1]
+        gradesRounded.append(min(sevenStep, key=lambda x: abs(x-grade)))
     return gradesRounded
 
 
-print(roundGrade(np.array([1, 2, 3, 4, 5, 6, 7])))
